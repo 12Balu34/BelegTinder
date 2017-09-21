36e4458102ecb1b213293e8925893b1bfd72e614
Christoph Welcz <christoph@welcz.de>
2017-09-21 15:41:30 +0200

    parametrized Belegkreis in categorize component

--- src/app/categorize/categorize.component.ts
+++ src/app/categorize/categorize.component.ts
@@ -1,15 +1,22 @@
-import { Component, OnInit } from '@angular/core';
+import {Component, OnInit} from "@angular/core";
+import {ActivatedRoute, Params} from "@angular/router";
 
 @Component({
-  selector: 'app-categorize',
-  templateUrl: './categorize.component.html',
-  styleUrls: ['./categorize.component.css']
+  selector: "app-categorize",
+  templateUrl: "./categorize.component.html",
+  styleUrls: ["./categorize.component.css"],
 })
 export class CategorizeComponent implements OnInit {
+  type: string;
 
-  constructor() { }
-
-  ngOnInit() {
+  constructor(private route: ActivatedRoute) {
   }
 
+  ngOnInit() {
+    this.refreshRouterParameters(this.route.snapshot.params);
+  }
+
+  private refreshRouterParameters(params: Params) {
+    this.type = params.type;
+  }
 }
