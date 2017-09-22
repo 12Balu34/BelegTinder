5d4fc16216f360239da974743a4e1b6e10cf5649
Christoph Welcz <christoph@welcz.de>
2017-09-22 09:52:46 +0200

    added hammer js

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,14 +1,26 @@
-import {BrowserModule} from "@angular/platform-browser";
-import {NgModule} from "@angular/core";
+import {BrowserModule, HAMMER_GESTURE_CONFIG, HammerGestureConfig} from '@angular/platform-browser';
+import {NgModule} from '@angular/core';
 
-import {AppRoutingModule} from "./app-routing.module";
-import {AppComponent} from "./app.component";
-import {CategorizeComponent} from "./categorize/categorize.component";
-import {DashboardComponent} from "./dashboard/dashboard.component";
-import {FormsModule} from "@angular/forms";
-import {DocumentTypesService} from "./shared/document-types.service";
-import {ListComponent} from "./list/list.component";
-import {DocumentsService} from "./shared/documents.service";
+import {AppRoutingModule} from './app-routing.module';
+import {AppComponent} from './app.component';
+import {CategorizeComponent} from './categorize/categorize.component';
+import {DashboardComponent} from './dashboard/dashboard.component';
+import {FormsModule} from '@angular/forms';
+import {DocumentTypesService} from './shared/document-types.service';
+import {ListComponent} from './list/list.component';
+import {DocumentsService} from './shared/documents.service';
+
+export class MyHammerConfig extends HammerGestureConfig {
+  overrides = <any>{
+    // override hammerjs default configuration
+    'pan': {threshold: 5},
+    'swipe': {
+      velocity: 0.4,
+      threshold: 20,
+      direction: 31 // /!\ ugly hack to allow swipe in all direction
+    },
+  };
+}
 
 @NgModule({
   declarations: [
@@ -22,7 +34,14 @@ import {DocumentsService} from "./shared/documents.service";
     AppRoutingModule,
     FormsModule,
   ],
-  providers: [DocumentTypesService, DocumentsService],
+  providers: [
+    DocumentTypesService,
+    DocumentsService,
+    {
+      provide: HAMMER_GESTURE_CONFIG,
+      useClass: MyHammerConfig
+    }
+  ],
   bootstrap: [AppComponent],
 })
 export class AppModule {
--- src/app/dashboard/dashboard.component.ts
+++ src/app/dashboard/dashboard.component.ts
@@ -1,13 +1,25 @@
-import { Component, OnInit } from '@angular/core';
+import {Component, OnInit} from '@angular/core';
 
 @Component({
   selector: 'app-dashboard',
   templateUrl: './dashboard.component.html',
-  styleUrls: ['./dashboard.component.css']
+  styleUrls: ['./dashboard.component.css'],
 })
 export class DashboardComponent implements OnInit {
 
-  constructor() { }
+  SWIPE_ACTION = {
+    LEFT: 'swipeleft',
+    RIGHT: 'swiperight',
+    UP: 'swipeup',
+    DOWN: 'swipeup',
+  };
+
+  constructor() {
+  }
+
+  swipe(action = this.SWIPE_ACTION.RIGHT) {
+    console.log('swipe it baby!' + action);
+  }
 
   ngOnInit() {
   }
