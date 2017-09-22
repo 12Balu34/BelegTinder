a6b9e10f416776eeb0a721e7812049007858c921
Christoph Welcz <christoph@welcz.de>
2017-09-22 10:16:30 +0200

    easified code

--- src/app/dashboard/dashboard.component.ts
+++ src/app/dashboard/dashboard.component.ts
@@ -8,14 +8,7 @@ import {Router} from '@angular/router';
 })
 export class DashboardComponent implements OnInit {
 
-  SWIPE_ACTION = {
-    LEFT: 'swipeleft',
-    RIGHT: 'swiperight',
-    UP: 'swipeup',
-    DOWN: 'swipeup',
-  };
-
-  private selectCategory = [
+   private selectCategory = [
     {swipe: 'left', category: 'outgoing_invoice'},
     {swipe: 'right', category: 'incoming_invoice'},
     {swipe: 'up', category: 'cash_register'},
@@ -25,7 +18,7 @@ export class DashboardComponent implements OnInit {
   constructor(private router: Router) {
   }
 
-  swipe(action = this.SWIPE_ACTION.RIGHT) {
+  swipe(action) {
     console.log('swipe it baby!' + action);
     const [category] = this.selectCategory
       .filter(d => 'swipe' + d.swipe === action)
