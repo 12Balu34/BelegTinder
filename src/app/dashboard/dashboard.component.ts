bca637ebda38916c3f851feddc0c8f4dfd7a54c7
Christoph Welcz <christoph@welcz.de>
2017-09-22 10:15:14 +0200

    implemented swipe gestures right

--- src/app/dashboard/dashboard.component.ts
+++ src/app/dashboard/dashboard.component.ts
@@ -1,4 +1,5 @@
 import {Component, OnInit} from '@angular/core';
+import {Router} from '@angular/router';
 
 @Component({
   selector: 'app-dashboard',
@@ -14,11 +15,22 @@ export class DashboardComponent implements OnInit {
     DOWN: 'swipeup',
   };
 
-  constructor() {
+  private selectCategory = [
+    {swipe: 'left', category: 'outgoing_invoice'},
+    {swipe: 'right', category: 'incoming_invoice'},
+    {swipe: 'up', category: 'cash_register'},
+    {swipe: 'down', category: 'other'},
+  ];
+
+  constructor(private router: Router) {
   }
 
   swipe(action = this.SWIPE_ACTION.RIGHT) {
     console.log('swipe it baby!' + action);
+    const [category] = this.selectCategory
+      .filter(d => 'swipe' + d.swipe === action)
+      .map(d => d.category);
+    this.router.navigate(['/categorize', category]);
   }
 
   ngOnInit() {
